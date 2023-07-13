3392e981e766194c0f6f960568cc53b24726695c
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-07-13 20:53:58 +0300

    add i18n configuration

--- src/config/i18n.tsx
+++ src/config/i18n.tsx
@@ -1,22 +1,16 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import i18n from 'i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import Backend from 'i18next-http-backend'
+import { initReactI18next } from 'react-i18next'
 
 i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
   .init({
-    resources: {
-      en: {
-        translation: {
-          "Welcome to React": "Welcome to React and react-i18next"
-        }
-      }
-    },
-    lng: "tr",
-    fallbackLng: "tr",
-
-    
+    fallbackLng: 'tr',
     interpolation: {
-      escapeValue: false
-    }
-  });
-  export default i18n;
+      escapeValue: false,
+    },
+  })
+export default i18n
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
-import "@turistikrota/ui/assets/config.css";
-import ContentLoader from "@turistikrota/ui/loader";
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {
-  RouterProvider
-} from "react-router-dom";
-import './index.css';
-import { router } from "./router/router";
+import '@turistikrota/ui/assets/config.css'
+import ContentLoader from '@turistikrota/ui/loader'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import './config/i18n'
+import './index.css'
+import { router } from './router/router'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider  router={router} fallbackElement={<ContentLoader />} />
+    <RouterProvider router={router} fallbackElement={<ContentLoader />} />
   </React.StrictMode>,
 )
