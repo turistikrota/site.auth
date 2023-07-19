a5998bbeff736438c77e315b290fc5dc36affacc
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-07-20 00:25:03 +0300

    complete on login action

--- src/utils/on-login.ts
+++ src/utils/on-login.ts
@@ -1,4 +1,6 @@
+const defaultHost: string = import.meta.env.VITE_REDIRECT_DEFAULT_HOST
+
 export const openRedirectUrl = (redirectable: boolean, redirectUrl: string | null) => {
-  const cb = redirectable && !!redirectUrl ? redirectUrl : 'https://turistikrota.com'
+  const cb = redirectable && !!redirectUrl ? redirectUrl : defaultHost
   window.open(cb, '_self')
 }
