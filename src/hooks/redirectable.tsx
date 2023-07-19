35f0b7f58c7404403b59c251fc55cfd172443443
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-07-19 23:57:00 +0300

    redirect: complete action

--- src/hooks/redirectable.tsx
+++ src/hooks/redirectable.tsx
@@ -3,7 +3,7 @@ import { useSearchParams } from 'react-router-dom'
 
 type CheckResult = [boolean, string | null]
 
-const whiteList = '.turistikrota.com'
+const whiteList: string[] = import.meta.env.VITE_REDIRECT_HOSTS.split(',')
 
 export const useRedirectable = (): CheckResult => {
   const [searchParams, setSearchParams] = useSearchParams()
@@ -13,11 +13,13 @@ export const useRedirectable = (): CheckResult => {
     const redirect = searchParams.get('redirect')
     if (redirect) {
       const url = new URL(redirect)
-      if (url.hostname.endsWith(whiteList)) {
+      if (whiteList.some((host) => url.hostname.endsWith(host))) {
         setUrl(redirect)
-        searchParams.delete('redirect')
-        setSearchParams(searchParams)
+      } else {
+        setUrl(null)
       }
+      searchParams.delete('redirect')
+      setSearchParams(searchParams)
     }
   }, [searchParams])
   return [!!url, url]
