3a4f01cbce9b7fd722aa77d9ee036a8334e3dc81
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-07-20 22:27:30 +0300

    add 404 view

--- src/config/i18n.tsx
+++ src/config/i18n.tsx
@@ -3,12 +3,21 @@ import LanguageDetector from 'i18next-browser-languagedetector'
 import Backend from 'i18next-http-backend'
 import { initReactI18next } from 'react-i18next'
 
+const detectLocale = (): string => {
+  const host = window.location.host
+  const subdomain = host.split('.')[0]
+  if (subdomain === 'giris') return 'tr'
+  return 'en'
+}
+
+console.log('locale::', detectLocale())
+
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'tr',
+    fallbackLng: detectLocale(),
     supportedLngs: ['tr', 'en'],
     interpolation: {
       escapeValue: false,
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,24 +1,29 @@
-import { createBrowserRouter } from "react-router-dom";
+import NotFoundView from '@/views/404'
+import { createBrowserRouter } from 'react-router-dom'
 
 export const router = createBrowserRouter([
   {
-    path: "/",
-    lazy: () => import("@/views/login.tsx")
+    path: '/',
+    lazy: () => import('@/views/login.tsx'),
   },
   {
-    path: "/activate/re-send-code",
-    lazy: () => import("@/views/re-send-code.tsx")
+    path: '/activate/re-send-code',
+    lazy: () => import('@/views/re-send-code.tsx'),
   },
   {
-    path: "/activate/:code",
-    lazy: () => import("@/views/activate.tsx")
+    path: '/activate/:code',
+    lazy: () => import('@/views/activate.tsx'),
   },
   {
-    path: "/etkinlestir/tekrar-gonder",
-    lazy: () => import("@/views/re-send-code.tsx")
+    path: '/etkinlestir/tekrar-gonder',
+    lazy: () => import('@/views/re-send-code.tsx'),
   },
   {
-    path: "/etkinlestir/:code",
-    lazy: () => import("@/views/activate.tsx")
+    path: '/etkinlestir/:code',
+    lazy: () => import('@/views/activate.tsx'),
   },
-]);
+  {
+    path: '*',
+    element: <NotFoundView />,
+  },
+])
