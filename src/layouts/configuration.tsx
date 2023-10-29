2c73fad78d17abce0bbc11d8b2ab19e8527875c5
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-10-29 16:05:32 +0300

    feature/design: update the glass effect

--- src/layouts/configuration.tsx
+++ src/layouts/configuration.tsx
@@ -25,7 +25,7 @@ export default function ConfigurationLayout({ children, page }: React.PropsWithC
             <Link to={`https://turistikrota.com/${i18n.language}`} target='_blank' className='flex items-center mb-6'>
               <Logo />
             </Link>
-            <GlassEffect justify='center' align='center' position=''>
+            <GlassEffect justify='center' align='center'>
               <GlassEffect.Item color='bg-primary' size='lg' position='-ml-20 mt-60' />
               <GlassEffect.Item color='bg-secondary' size='xl' position='ml-10 mt-20' />
             </GlassEffect>
