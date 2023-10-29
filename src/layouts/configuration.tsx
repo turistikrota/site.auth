bd9e6848d33ba6e523354b8fe3b35c45ccde9abd
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-10-29 15:58:46 +0300

    feature/design: update the design

--- src/layouts/configuration.tsx
+++ src/layouts/configuration.tsx
@@ -1,3 +1,4 @@
+import GlassEffect from '@turistikrota/ui/design/glass'
 import Logo from '@turistikrota/ui/logo'
 import { ToastListProvider, ToastProvider } from '@turistikrota/ui/toast'
 import React from 'react'
@@ -20,12 +21,14 @@ export default function ConfigurationLayout({ children, page }: React.PropsWithC
     <ToastListProvider>
       <ToastProvider>
         <section className='h-full'>
-          <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto h-full lg:py-0'>
+          <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto h-full lg:py-0 relative'>
             <Link to={`https://turistikrota.com/${i18n.language}`} target='_blank' className='flex items-center mb-6'>
-              <Logo>
-                <Logo.Badge>beta</Logo.Badge>
-              </Logo>
+              <Logo />
             </Link>
+            <GlassEffect justify='center' align='center' position=''>
+              <GlassEffect.Item color='bg-primary' size='lg' position='-ml-20 mt-60' />
+              <GlassEffect.Item color='bg-secondary' size='xl' position='ml-10 mt-20' />
+            </GlassEffect>
             <div className='w-full bg-second shadow-lg rounded-lg md:mt-0 sm:max-w-md xl:p-0'>{children}</div>
           </div>
         </section>
